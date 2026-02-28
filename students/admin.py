23b8c86abf5bf662f50250e564364dac25742f7f
Abhiraj Pathak <abhiraj.pathak3009@gmail.com>
2026-02-28 16:48:09 +0530

    Successfully implemented Teacher model and database migration

--- students/admin.py
+++ students/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Teacher
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
@@ -8,3 +8,11 @@ class StudentAdmin(admin.ModelAdmin):
     
     # This adds a search bar
     search_fields = ('first_name', 'last_name', 'roll_number')
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    # Displays these columns for Teachers
+    list_display = ('first_name', 'last_name', 'subject', 'email')
+    
+    # Allows you to search by name or their specialty subject
+    search_fields = ('first_name', 'last_name', 'subject')
--- students/models.py
+++ students/models.py
@@ -10,3 +10,13 @@ class Student(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    subject = models.CharField(max_length=100)
+    hire_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Prof. {self.last_name} ({self.subject})"
